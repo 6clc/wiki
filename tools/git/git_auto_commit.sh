ab5102a5f25d9851fe818c1fe31d3ea6a7ac5c75
liuchao <liuchao.lc@qq.com>
2021-01-26 21:08:36 +0800

    shell auto commit on ${commit_date}

---
+++ tools/git/git_auto_commit.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+# https://broqiang.github.io/2018/01/20/git-auto-commit-on-shutdown/
+
+cz_dir=~/wiki
+
+commit_date=$(date +%Y-%m-%d_%H-%M-%S)
+
+action=$1
+
+# 自动提交函数
+for cur_dir in ${cz_dir}
+do
+  echo $cur_dir
+  pushd $cur_dir
+  if [[ "$action" == "start" ]]; then
+    git pull
+  fi
+
+  if [[ "$action" == "stop" ]]; then
+    git add .
+    git commit -m 'shell auto commit on ${commit_date}'
+    git push
+  fi
+  popd
+done
